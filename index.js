f3a7a8e0a8d3715b7d58b954f94e86bb7ad4bc67
goguamapark <a01062684417@gmail.com>
2026-05-20 15:19:52 +0900

    add delete log

--- index.js
+++ index.js
@@ -7,12 +7,32 @@ const {
     EmbedBuilder
 } = require('discord.js');
 
+const deletedMessages = new Map();
 const token = process.env.TOKEN;
 const clientId = '1506507365560877156';
 const guildId = '1172129810861015131';
 
+client.on('messageDelete', message => {
+
+    // 봇 메시지 무시
+    if (message.author?.bot) return;
+
+    deletedMessages.set(message.channel.id, {
+
+        author: message.author.tag,
+        content: message.content || '(내용 없음)',
+        createdAt: new Date()
+
+    });
+
+});
+
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
 const userFortunes = {};
@@ -32,7 +52,22 @@ const commands = [
 
     new SlashCommandBuilder()
         .setName('운세')
-        .setDescription('오늘의 운세를 알려줍니다')
+        .setDescription('오늘의 운세를 알려줍니다'),
+
+    new SlashCommandBuilder()
+    .setName('청소')
+    .setDescription('메시지를 삭제합니다')
+    .addIntegerOption(option =>
+        option
+            .setName('개수')
+            .setDescription('삭제할 메시지 개수')
+            .setRequired(true)
+    ),
+
+    new SlashCommandBuilder()
+    .setName('삭제로그')
+    .setDescription('최근 삭제된 메시지를 확인합니다')
+    
 ]
 .map(command => command.toJSON());
 
@@ -118,7 +153,7 @@ client.on('interactionCreate', async interaction => {
     if (interaction.commandName === '소개') {
 
         const embed = new EmbedBuilder()
-            .setTitle('🐝 양봉이 봇')
+            .setTitle('🐝 양봉이')
             .setDescription(
                 '안녕 날 소개하지 난 양봉장의 전용 봇 양봉이라고 하오'
             )
@@ -127,7 +162,7 @@ client.on('interactionCreate', async interaction => {
                 'https://cdn.discordapp.com/attachments/1110460136373366845/1506536312423841873/image.png?ex=6a0e9ec6&is=6a0d4d46&hm=df046c8c3c4fd195fbe36ebaa666f13d010f5be1b03090e878b5b53b8276c237&'
             )
             .setFooter({
-                text: '양봉장 봇 시스템'
+                text: '양봉장의 전용 봇, 아이스크림을 굉장히 좋아한다.'
             });
 
         await interaction.reply({
@@ -136,7 +171,73 @@ client.on('interactionCreate', async interaction => {
 
     }
 
+if (interaction.commandName === '청소') {
 
+    // 관리자 권한 체크
+    if (!interaction.member.permissions.has('ManageMessages')) {
+
+        return interaction.reply({
+            content: '❌ 메시지 관리 권한 없잖아!',
+            ephemeral: true
+        });
+
+    }
+
+    const amount =
+        interaction.options.getInteger('개수');
+
+    // 1~100 제한
+    if (amount < 1 || amount > 100) {
+
+        return interaction.reply({
+            content: '❌ 1~100개만 삭제 가능함 ㅇㅇ.;;;',
+            ephemeral: true
+        });
+
+    }
+
+    await interaction.channel.bulkDelete(amount, true);
+
+    await interaction.reply({
+        content: `🧹 메시지 ${amount}개 삭제 완료!`,
+        ephemeral: true
+    });
+
+}
+
+if (interaction.commandName === '삭제로그') {
+
+    const data =
+        deletedMessages.get(interaction.channel.id);
+
+    if (!data) {
+
+        return interaction.reply({
+            content: '❌ 최근 삭제된 메시지가 없다!',
+            ephemeral: true
+        });
+
+    }
+
+    const embed = new EmbedBuilder()
+        .setTitle('🗑 최근 삭제된 메시지')
+        .addFields(
+            {
+                name: '작성자',
+                value: data.author
+            },
+            {
+                name: '내용',
+                value: data.content
+            }
+        )
+        .setColor('Red');
+
+    await interaction.reply({
+        embeds: [embed]
+    });
+
+}
 
 });
 
