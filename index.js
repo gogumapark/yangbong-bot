51466b0f4c0caae184db03cd9c595bd722ae9a43
goguamapark <a01062684417@gmail.com>
2026-05-20 16:23:14 +0900

    fix warnings

--- index.js
+++ index.js
@@ -394,16 +394,3 @@ if (isDraw) {
 });
 
 client.login(token);
-
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Bot is running');
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Web server running on port ${PORT}`);
-});
