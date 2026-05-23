99282b7a6b19616f278a9e7becda01460e657a07
goguamapark <a01062684417@gmail.com>
2026-05-24 01:58:02 +0900

    fix warnings

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ mongoose.connect(process.env.MONGO_URI)
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
     const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash"
+        model:gemini-1.5-flash
     });
 
     const Stock = mongoose.model('Stock', stockSchema);
